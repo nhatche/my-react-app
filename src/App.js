5cdc515fd2378a7cfa0e02953687195e5df9a09c
quang nhat <quangnu2110@gmail.com>
2021-12-02 21:09:31 +0700

    msi device commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 import Header1 from './components/Header1';
 
 class App extends Component {
-  render(){
-      return (
-          <div>
-              <Header1/>
-              hello nhat
-              hello cin
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div>
+                <Header1 />
+                hello nhat
+                hello cin
+                hello nu
+            </div>
+        );
+    }
 }
 
 export default App;
